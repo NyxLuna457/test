68b681b8e423e3efaa9440f5cd8ba35dfb63c05d
nyx <nyx@artyfetes.com>
2025-05-13 16:14:20 +0200

    commit3

--- app.py
+++ app.py
@@ -9,4 +9,21 @@ def home ():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-     
+
+import mariadb
+import sys
+
+try:
+    # Remplace les valeurs ci-dessous par tes identifiants
+    conn = mariadb.connect(
+        user="root",
+        password="tssr14*",
+        host="localhost",      # ou l'IP de l'hôte si ce n'est pas sur la même machine
+        port=3306,            # port par défaut de MariaDB
+        database="TA_BASE"    # optionnel, selon ta configuration
+    )
+    print("yes")
+    conn.close()
+except mariadb.Error as e:
+    print(f"Erreur lors de la connexion à MariaDB : {e}")
+    sys.exit(1)
