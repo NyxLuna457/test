65c07946652dd5cbdfe662060219959a62c2b9c0
nyx <nyx@artyfetes.com>
2025-05-14 09:17:08 +0200

    commit5

--- app.py
+++ app.py
@@ -12,18 +12,19 @@ if __name__ == '__main__':
 
 import mariadb
 import sys
-
+db_config = {        
+        'user' : 'root',
+        'password' : 'tssr14*',
+        'host' : 'localhost',      # ou l'IP de l'hôte si ce n'est pas sur la même machine
+        'port' : '3306',            # port par défaut de MariaDB
+        'database' : 'NYXDB'    # optionnel, selon ta configuration
+     }
 try:
     # Remplace les valeurs ci-dessous par tes identifiants
-    conn = mariadb.connect(
-        user="root",
-        password="tssr14*",
-        host="localhost",      # ou l'IP de l'hôte si ce n'est pas sur la même machine
-        port=3306,            # port par défaut de MariaDB
-        database="NYXDB"    # optionnel, selon ta configuration
-    )
+    conn = mariadb.connect(**db_config)
+    conn.ping()
     print("yes")
     conn.close()
-except mariadb.Error as e:
-    print(f"Erreur lors de la connexion à MariaDB : {e}")
-    sys.exit(1)
+except mariadb.Error as err:
+    print(f"Erreur lors de la connexion à MariaDB : {err}")
+    
